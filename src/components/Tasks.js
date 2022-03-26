463e1dd7cbc3c6fce6a599eb3926c4222f4b1c44
Roha <rohanwaz118@gmail.com>
2022-03-25 19:33:21 -0900

    Setup mutltiple roles for react app

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,11 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-  ]
+import Task from './Task'
 
-
-
-const Tasks = () => {
-  return (
+const Tasks = ( {tasks} ) => {
+    return (
     <div>
-      
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
     </div>
   )
 }
