1e05037694f16c442c941337f0dd34549f738ad4
Roha <rohanwaz118@gmail.com>
2022-03-26 18:03:22 -0900

    Delete task,toggle reminder & add task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -23,10 +24,31 @@ const App = () => {
       reminder: false,
     }
   ])
-  return (
+
+// Delete Task
+   const deleteTask = (id) => {
+     setTasks(tasks.filter((task) => task.id !== id))
+   }
+
+// Toggle remider
+   const toggleReminder = (id) => {
+     setTasks(tasks.map((task) => 
+     task.id === id ? { ...task, reminder: 
+      !task.reminder } : task
+      )
+      )
+   }   
+
+    return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   );
 }
